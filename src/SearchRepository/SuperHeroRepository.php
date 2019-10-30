4880d81c33209b0bc6e4d39bbdb8d66825316230
Viktor Breitenbücher <breitenbuecher.viktor@gmail.com>
2019-10-30 20:30:38 +0700

    Add another fuzzy example.

--- src/SearchRepository/SuperHeroRepository.php
+++ src/SearchRepository/SuperHeroRepository.php
@@ -8,16 +8,20 @@ use FOS\ElasticaBundle\Repository;
 
 class SuperHeroRepository extends Repository
 {
-    public function search($search = null, $limit = 10)
+    public function search($search = null, $limit = 25)
     {
         $query = new Query();
 
         $boolQuery = new BoolQuery();
 
         if (!\is_null($search)) {
-            $fieldQuery = new Query\Fuzzy();
-            $fieldQuery->setField('name', $search);
-
+            $fieldQuery = new Query\Match('name', [
+                "query" =>         $search,
+                "boost" =>         1.0,
+                "fuzziness" =>     2,
+                "prefix_length" => 0,
+                "max_expansions" => 100
+            ]);
             $boolQuery->addMust($fieldQuery);
         }
 
